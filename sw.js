755f98a12717757c5dff3c97abf74f8cfa1d4578
Deepak Kumar <deepak.csjm@gmail.com>
2026-07-04 13:01:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open('brn-v0.1').then(cache => cache.addAll([
+    './',
+    './index.html'
+  ])));
+});
 
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
